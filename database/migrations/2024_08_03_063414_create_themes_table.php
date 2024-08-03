1b2bed55eb58bae087320c799bb2f1acaba193aa
nayan97 <nayanislam085@gmail.com>
2024-08-03 13:05:17 +0600

    data save for site settings

--- database/migrations/2024_08_03_063414_create_themes_table.php
+++ database/migrations/2024_08_03_063414_create_themes_table.php
@@ -13,6 +13,16 @@ return new class extends Migration
     {
         Schema::create('themes', function (Blueprint $table) {
             $table->id();
+            $table->string('logo');
+            $table->string('running_tag') ->nullable();
+            $table->string('email') ->nullable();
+            $table->string('cell') ->nullable();
+            $table->string('address') ->nullable();
+            $table->string('copyright');
+            $table->text('social');
+            $table->boolean('search') -> default(false);
+            $table->string('title');
+            $table->string('tagline');
             $table->timestamps();
         });
     }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Theme;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +19,16 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        Theme::create([
+
+            'logo'              => 'logo.png',
+            'copyright'         => '2024 Fresh Mart Agency',
+            'running_tag'       => 'Free Shipping for all Order of $99',
+            'social'            => '[]',
+            'title'             => 'Fresh Mart',
+            'tagline'           => 'Take Fresh Be Healthy',
+            
+            ]);
     }
 }
